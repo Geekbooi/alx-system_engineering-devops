78608f0aa98a4f72887420d11422ae1433bad715
Geekbooi <yosephmekonnen@gmail.com>
2022-09-19 11:28:39 +0300

    SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
-# Manifest to modify SSH config file
+# Puppet to make changes to our configuration file
+
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no'
-}
+  line   => 'PasswordAuthentication no',
+  }
+
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/holberton'
-}
+  line   => 'IdentityFile ~/.ssh/school',
+  }
